6c07d6f0d1c3ec1bce3677d1baf20d40822bdd90
Blake Patterson <41084151+Bpatt9690@users.noreply.github.com>
2022-08-16 17:28:55 -0400

    Update Elevation.py

--- Mark_IV/Elevation_Dev/Elevation.py
+++ Mark_IV/Elevation_Dev/Elevation.py
@@ -5,27 +5,20 @@ from datetime import date, datetime
 import pytz
 from Kalman import KalmanAngle
 import smbus
-import time
-import math
 import RPi.GPIO as GPIO
 from time import sleep
+
 GPIO.setwarnings(False) 
-
 gps_dict = {}
-
-#Get current date
-today = date.today()
-year = int(today.strftime("%Y"))
-day = int(today.strftime("%d"))
-month = int(today.strftime("%m").replace("0",""))
-
 bus = smbus.SMBus(1) 
+
 DeviceAddress = 0x68 
-RestrictPitch = True    #Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+RestrictPitch = True 
 radToDeg = 57.2957786
 kalAngleX = 0
 kalAngleY = 0
-#some MPU6050 Registers and their Address
+
+#MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
 CONFIG       = 0x1A
@@ -46,7 +39,7 @@ def gpsData():
 
     while True:
         line = gps.readline()
-        print('Data:',line)
+        logs('GPS Data:',line)
      
         try:
             line = line.decode("utf-8")
@@ -63,7 +56,8 @@ def gpsData():
                 break
 
         except:
-            print("Byte recieve issue")
+            logs("GPS Data Issue")
+            logs("Manually Inserting Data for Testing")
             gps_dict['Time UTC'] = '181735.00'
             gps_dict['Lattitude'] = float(2833.2327)/100
             gps_dict['Lattitude Direction'] = 'N'
@@ -76,23 +70,23 @@ def gpsData():
 
 def sunpos(when, location, refraction):
 
-# Extract the passed data
+    #Extract the passed data
     year, month, day, hour, minute, second, timezone = when
     latitude, longitude = location
 
-# Math typing shortcuts
+    #Math typing shortcuts
     rad, deg = math.radians, math.degrees
     sin, cos, tan = math.sin, math.cos, math.tan
     asin, atan2 = math.asin, math.atan2
 
-# Convert latitude and longitude to radians
+    #Convert latitude and longitude to radians
     rlat = rad(latitude)
     rlon = rad(longitude)
 
-# Decimal hour of the day at Greenwich
+    #Decimal hour of the day at Greenwich
     greenwichtime = hour - timezone + minute / 60 + second / 3600
 
-# Days from J2000, accurate from 1901 to 2099
+   #Days from J2000, accurate from 1901 to 2099
     daynum = (
         367 * year
         - 7 * (year + (month + 9) // 12) // 4
@@ -102,98 +96,82 @@ def sunpos(when, location, refraction):
         + greenwichtime / 24
     )
 
-# Mean longitude of the sun
+    #Mean longitude of the sun
     mean_long = daynum * 0.01720279239 + 4.894967873
 
-# Mean anomaly of the Sun
+    #Mean anomaly of the Sun
     mean_anom = daynum * 0.01720197034 + 6.240040768
 
-# Ecliptic longitude of the sun
+    #Ecliptic longitude of the sun
     eclip_long = (
         mean_long
         + 0.03342305518 * sin(mean_anom)
         + 0.0003490658504 * sin(2 * mean_anom)
     )
 
-# Obliquity of the ecliptic
+    #Obliquity of the ecliptic
     obliquity = 0.4090877234 - 0.000000006981317008 * daynum
 
-# Right ascension of the sun
+    #Right ascension of the sun
     rasc = atan2(cos(obliquity) * sin(eclip_long), cos(eclip_long))
-# Declination of the sun
+
+    #Declination of the sun
     decl = asin(sin(obliquity) * sin(eclip_long))
-# Local sidereal time
+
+    #Local sidereal time
     sidereal = 4.894961213 + 6.300388099 * daynum + rlon
-# Hour angle of the sun
+
+    #Hour angle of the sun
     hour_ang = sidereal - rasc
-# Local elevation of the sun
+
+    #Local elevation of the sun
     elevation = asin(sin(decl) * sin(rlat) + cos(decl) * cos(rlat) * cos(hour_ang))
-# Local azimuth of the sun
+
+    #Local azimuth of the sun
     azimuth = atan2(
         -cos(decl) * cos(rlat) * sin(hour_ang),
         sin(decl) - sin(rlat) * sin(elevation),
     )
-# Convert azimuth and elevation to degrees
+
+    #Convert azimuth and elevation to degrees
     azimuth = into_range(deg(azimuth), 0, 360)
     elevation = into_range(deg(elevation), -180, 180)
-# Refraction correction (optional)
+
+    #Refraction correction (optional)
     if refraction:
         targ = rad((elevation + (10.3 / (elevation + 5.11))))
         elevation += (1.02 / tan(targ)) / 60
-# Return azimuth and elevation in degrees
+
+    #Return azimuth and elevation in degrees
     return (round(azimuth, 2), round(elevation, 2))
 def into_range(x, range_min, range_max):
     shiftedx = x - range_min
     delta = range_max - range_min
     return (((shiftedx % delta) + delta) % delta) + range_min
 
-
-
-def logs(str):
-    #log timestamp,event, process
-    print(str)
-    f = open("gpsData.txt", "a")
-    f.write(str)
-    f.close()
-
 def read_raw_data(addr):
     #Accelero and Gyro value are 16-bit
+    high = bus.read_byte_data(DeviceAddress, addr)
+    low = bus.read_byte_data(DeviceAddress, addr+1)
 
-    while(1):
-        try:
-            high = bus.read_byte_data(DeviceAddress, addr)
-            low = bus.read_byte_data(DeviceAddress, addr+1)
+    #concatenate higher and lower value
+    value = ((high << 8) | low)
 
-            #concatenate higher and lower value
-            value = ((high << 8) | low)
+    #to get signed value from mpu6050
+    if(value > 32768):
+            value = value - 65536
 
-            #to get signed value from mpu6050
-            if(value > 32768):
-                    value = value - 65536
-
-            return value
-        except:
-            pass
-
+    return value
 
  #Read the gyro and acceleromater values from MPU6050
 def MPU_Init():
     time.sleep(1)
     #write to sample rate register
     bus.write_byte_data(DeviceAddress, SMPLRT_DIV, 7)
-
-    #Write to power management register
-
     bus.write_byte_data(DeviceAddress, PWR_MGMT_1, 1)
-
-    #Write to Configuration register
-    #Setting DLPF (last three bit of 0X1A to 6 i.e '110' It removes the noise due to vibration.) https://ulrichbuschbaum.wordpress.com/2015/01/18/using-the-mpu6050s-dlpf/
-
     bus.write_byte_data(DeviceAddress, CONFIG, int('0000110',2))
-
     #Write to Gyro configuration register
     bus.write_byte_data(DeviceAddress, GYRO_CONFIG, 24)
-
     #Write to interrupt enable register
     bus.write_byte_data(DeviceAddress, INT_ENABLE, 1)
 
@@ -204,15 +182,11 @@ def tiltAngle():
    
     MPU_Init()
 
-    time.sleep(1)
     #Read Accelerometer raw value
     accX = read_raw_data(ACCEL_XOUT_H)
     accY = read_raw_data(ACCEL_YOUT_H)
     accZ = read_raw_data(ACCEL_ZOUT_H)
 
-    #print('here')
-    #print(accX,accY,accZ)
-    #print(math.sqrt((accY**2)+(accZ**2)))
     if (RestrictPitch):
         roll = math.atan2(accY,accZ) * radToDeg
         pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
@@ -295,17 +269,18 @@ def tiltAngle():
 
             if ((gyroXAngle < -180) or (gyroXAngle > 180)):
                 gyroXAngle = kalAngleX
+            if ((gyroYAngle < -180) or (gyroYAngle > 180)):
+                gyroYAngle = kalAngleY
 
-            return str(kalAngleX)
+            return str(kalAngleX), str(kalAngleY)
             time.sleep(0.5)
 
         except Exception as exc:
             flag += 1
 
 
-def elevationMovement(elevation):
+def solarTracking(elevation):
 
-    #GPIO Setup
 
     # Direction pin from controller
     AZ_DIR = 25
@@ -317,104 +292,100 @@ def elevationMovement(elevation):
     CW = 0
     CCW = 1
 
+    #Should be set by user, either via flag or direct input
     accuracy = 10.0
 
-    # Setup pin layout on PI
+    # Setup pin layout on RPI
     GPIO.setmode(GPIO.BCM)
-
-    # Establish Pins in software
     GPIO.setup(AZ_DIR, GPIO.OUT)
     GPIO.setup(AZ_STEP, GPIO.OUT)
 
-    #gathering current tilt angle from sensor
-    currentTiltAngle = 90 - float(tiltAngle())
-    #bypassing for testing reasons
+    currentTiltAngleX,currentTiltAngleY = tiltAngle()
 
-    #userInput = input("Enter angle:")
-    #print("Angle in use is: ",userInput)
-    #currentTiltAngle = str(userInput)
-    ###############
-    #currentTiltAngle = 9float(currentTiltAngle)
+
+    #Gathering current tilt angle from sensor
+    currentTiltAngleX = 90 - float(currentTiltAngleX)
+    currentTiltAngleY = 360 - float(currentTiltAngleY)
 
     logs("Current Tilt Angle: "+str(currentTiltAngle))
     logs("Current Solar Elevation: "+str(elevation))
     time.sleep(1)
 
-    #currentTiltAngle 
-
+    #Need to reevaluate whats happening here in testing
     #Used for angle adjustment
     if float(currentTiltAngle) < 0:
-        currentTiltAngle = 90 - float(currentTiltAngle) * (-1)
+        currentTiltAngleX = 90 - float(currentTiltAngleX) * (-1)
 
-    degreeDifference = float(elevation) - float(currentTiltAngle)
-    logs("Current Degree Difference: "+str(degreeDifference))
+    degreeDifferenceX = float(elevation) - float(currentTiltAngleX)
+
+    logs("Current Degree Difference in elevation: "+str(degreeDifferenceX))
     
     try:
 
-        while degreeDifference > accuracy or degreeDifference < 0:
-                # Setup pin layout on PI
-    
-            logs(" ")
+        while degreeDifferenceX > accuracy or degreeDifferenceX < 0:
+
             logs("Adjusting Elevation Angle")
-            logs("Current Degree Difference: "+str(degreeDifference))
+            logs("Current Degree Difference in elevation: "+str(degreeDifferenceX))
 
             sleep(1.0)
-            # Esablish the direction you want to go
-            if degreeDifference > 0:
+
+            if degreeDifferenceX > 0:
                 GPIO.output(AZ_DIR, CW)
                 logs("CW Rotation")
             else:
                 sleep(1.0)
                 GPIO.output(AZ_DIR,CCW)
-                print('here in reversal')
                 logs("CWW Rotation")
 
-                 # Run for 200 steps. This will change based on how you set you controller
+            # Run for 200 steps. This will change based on how you set you controller
             for x in range(200):
-
-                # Set one coil winding to high
                 GPIO.output(AZ_STEP,GPIO.HIGH)
-                # Allow it to get there.
                 sleep(.05) # Dictates how fast stepper motor will run
-                # Set coil winding to low
                 GPIO.output(AZ_STEP,GPIO.LOW)
 
-            
-            #new readings
-            currentTiltAngle = 90 - float(tiltAngle())
-            #bypassing for testing reasons
-            #userInput = input("Enter angle:")
-            #print("Angle in use is: ",userInput)
-            #currentTiltAngle = str(userInput)
-            ###############
-            degreeDifference = float(elevation) - float(currentTiltAngle)
+            #New Angle Readings
+            currentTiltAngleX, currentTiltAngleY = tiltAngle()
+            currentTiltAngleX = 90 - float(currentTiltAngleX)
+            currentTiltAngleY = 360 - float(currentTiltAngleY)
 
-        
-        logs('Tilt angle reached, stopping adjustment')
+            degreeDifferenceX = float(elevation) - float(currentTiltAngleX)
+            degreeDifferenceY = float(elevation) - float(currentTiltAngleY)
+
+        logs('Elevation Angle Reached, Stopping Adjustment')
 
     except KeyboardInterrupt:
-        print("cleanup")
+        logs("GPIO Cleanup")
         GPIO.cleanup()
 
 
+#Logging method, need to add timestamp and structure to logging objects
+def logs(str):
+    print("log entry: "+str)
+    f = open("gpsData.txt", "a")
+    f.write(str)
+    f.close()
+
+#Used to cleanup GPIO pins on a keyboard hault
 def cleanup():
-    print("cleanup")
+    logs("GPIO Pin cleanup")
     GPIO.cleanup()
     exit()
 
-
 def main():
-
-    global year, month, day
+    #Get current date
+    today = date.today()
+    year = int(today.strftime("%Y"))
+    day = int(today.strftime("%d"))
+    month = int(today.strftime("%m").replace("0",""))
 
     try:
         gpsData()
+
         logs(str(gps_dict))
 
         while True:
 
             now = datetime.utcnow().strftime("%H:%M:%S").replace(":","")
-            
             hour = str(now[0:2])
             minutes = str(now[2:4])
             seconds = str(now[4:6])
@@ -425,75 +396,24 @@ def main():
                 longitude = gps_dict['Longitude']
           
             location = (gps_dict['Lattitude'], longitude)
-            when = (year, month, day,int(hour),int(minutes),int(seconds), 0) #(year,month,date,hour,minue,sec,timezone adj)
+            when = (year, month, day,int(hour),int(minutes),int(seconds), 0)
        
             azimuth, elevation = sunpos(when, location, True)
 
             tz_NY = pytz.timezone('America/New_York') 
             datetime_NY = datetime.now(tz_NY)
 
+
+            #Logging current UTC/EST time and Solar Azimuth#
             logs("Current UTC: "+str(now))
             logs("EST time: "+str(datetime_NY.strftime("%H:%M:%S")))
             logs("Current Solar Azimuth: "+str(azimuth))
-            elevationMovement(elevation)
-            print()
 
-            time.sleep(1)
+            solarTracking(elevation)
 
     except KeyboardInterrupt:
-        print("cleanup")
+        logs("GPIO Cleanup")
         GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
-
-        
-
-
-
-
-                #changing dir
-            #GPIO.cleanup()
-            #   """Change Direction: Changing direction requires time to switch. The
-            #   time is dictated by the stepper motor and controller. """
-            #   sleep(1.0)
-            #   GPIO.output(DIR,CCW)
-            #   for x in range(200):
-            #       GPIO.output(STEP,GPIO.HIGH)
-            #       sleep(.005)
-            #       GPIO.output(STEP,GPIO.LOW)
-            #       sleep(.005)
-
-
-                #sleep(.005) # Dictates how fast stepper motor will run
-
-                #GPIO.output(AZ_STEP,GPIO.HIGH)
-
-                # Allow it to get there.
-                #sleep(.00001) # Dictates how fast stepper motor will run
-                # Set coil winding to low
-                #GPIO.output(AZ_STEP,GPIO.LOW)
-
-                #sleep(.005)
-
-
-    #   """Change Direction: Changing direction requires time to switch. The
-    #   time is dictated by the stepper motor and controller. """
-    #   sleep(1.0)
-    #   GPIO.output(DIR,CCW)
-    #   for x in range(200):
-    #       GPIO.output(STEP,GPIO.HIGH)
-    #       sleep(.005)
-    #       GPIO.output(STEP,GPIO.LOW)
-    #       sleep(.005)
-
-            
-            #sleep(.00001)
-            #GPIO.output(AZ_DIR, CW)
-            #GPIO.output(AZ_STEP,GPIO.HIGH)
-
-            #Allow it to get there.
-            #sleep(.0005) # Dictates how fast stepper motor will run
-            #Set coil winding to low
-            #GPIO.output(AZ_STEP,GPIO.LOW)
-            #Dictates how fast stepper motor will run
