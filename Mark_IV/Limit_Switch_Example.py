8c942d42d20f30d4cc5921d873137fea64ce099e
Bpatt9690 <pattersonsoftwareindustries@gmail.com>
2022-09-11 13:12:09 -0400

    hurry up

--- Mark_IV/Limit_Switch_Example.py
+++ Mark_IV/Limit_Switch_Example.py
@@ -1,3 +1,6 @@
+#x axis
+
+
 import RPi.GPIO as GPIO
 from time import sleep
 
@@ -8,5 +11,5 @@ GPIO.setup(switch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 while(1):
 	if GPIO.input(switch)==1:
 		print('Button was pressed')
-		sleep(.1)
+		sleep(1)
 	sleep(.1)
