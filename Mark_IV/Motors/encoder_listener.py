41ae9c0b5a85defb68686c08d16bb93d36b2c44a
Blake Patterson <41084151+Bpatt9690@users.noreply.github.com>
2023-03-24 14:36:37 -0400

    Create encoder_listener.py

---
+++ Mark_IV/Motors/encoder_listener.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+
+def listen_rising_edge(pin):
+    # Set up GPIO mode and pin
+    current pin = 21
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.IN)
+
+    # Set up edge detection
+    GPIO.add_event_detect(pin, GPIO.RISING)
+
+    # Wait for rising edge
+    while True:
+        if GPIO.event_detected(pin):
+            print("Rising edge detected on pin {}".format(pin))
+            break
+
+    # Clean up GPIO
+    GPIO.cleanup()
+
+def main():
+    listen_rising_edge(21)
+
+if __name__ == '__main__':
+    main()
