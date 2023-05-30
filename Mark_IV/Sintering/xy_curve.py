21f1c71884e3905bb7abdc3f9761bd6a900f6dbb
JuddB <jbrockedgar@gmail.com>
2023-05-30 11:08:43 -0400

    updated xy_curve.py, more usable for all curves and rotations.

--- Mark_IV/Sintering/xy_curve.py
+++ Mark_IV/Sintering/xy_curve.py
@@ -14,15 +14,6 @@ def xyCurve(x_dist=6, y_dist=6, x_circle=0, y_circle=6):
     num_segs = 10
     radius = sqrt(x_circle * x_circle + y_circle * y_circle)
 
-    # x and y rotate determine if motors move clockwise or counterclockwise.
-    # True for CW and False for CCW.
-    x_rotate = True
-    y_rotate = True
-    if x_dist < 0:
-        x_rotate = False
-    if y_dist < 0:
-        y_rotate = False
-
     # Get start and end angle on unit circle.
     if(x_circle == 0):
         start_ang = math.pi / 2
@@ -31,35 +22,45 @@ def xyCurve(x_dist=6, y_dist=6, x_circle=0, y_circle=6):
     end_ang = tan((y_circle - y_dist) / (x_circle + float(x_dist)))
     angle_delta = (start_ang - end_ang) / float(num_segs)
     theta = start_ang
+    if start_ang > end_ang:
+        angle_delta *= -1
 
-    y_max = radius * sin(theta)
+    x_prev = radius * abs(cos(theta))
+    y_prev = radius * abs(sin(theta))
 
     for _ in range(num_segs):
         # Get current angle on unit circle.
-        theta -= angle_delta
+        theta += angle_delta
 
-        x = abs(x_prev - radius * abs(cos(theta)))
-        y = abs(y_max - y_prev - radius * abs(sin(theta)))
+        x = x_prev - radius * abs(cos(theta))
+        y = y_prev - radius * abs(sin(theta))
 
         # Use angle to change the travel distance and speed for the segments.
         # NEEDS TO BE CHANGED
 
         x_speed = speed_mod
         y_speed = speed_mod
-        if x >= y and x != 0:
-            y_speed = speed_mod * (y / x)
-        elif x < y and y != 0:
-            x_speed = speed_mod * (x / y)
+        if abs(x) >= abs(y) and x != 0:
+            y_speed = speed_mod * abs(y / x)
+        elif abs(x) < abs(y) and y != 0:
+            x_speed = speed_mod * abs(x / y)
 
-        xProc = mp.Process(target=xMove, args=(x, x_rotate, x_speed))
-        yProc = mp.Process(target=yMove, args=(y, y_rotate, y_speed))
+        x_rotate = True
+        y_rotate = True
+        if (angle_delta < 0 and y > 0) or (angle_delta > 0 and y < 0):
+            x_rotate = False
+        if (angle_delta < 0 and x < 0) or (angle_delta > 0 and x > 0):
+            y_rotate = False
+
+        xProc = mp.Process(target=xMove, args=(abs(x), x_rotate, x_speed))
+        yProc = mp.Process(target=yMove, args=(abs(y), y_rotate, y_speed))
         xProc.start()
         yProc.start()
         xProc.join()
         yProc.join()
 
-        x_prev += x
-        y_prev += y
+        x_prev = radius * abs(cos(theta))
+        y_prev = radius * abs(sin(theta))
 
 
 def main():
