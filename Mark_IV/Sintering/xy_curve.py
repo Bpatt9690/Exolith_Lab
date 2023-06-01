c0afbeba3b1ebe23d97f78c43a01ec7a943800bb
JuddB <jbrockedgar@gmail.com>
2023-06-01 11:19:04 -0400

    accounts for arctan being undefined in xy_curve.py

--- Mark_IV/Sintering/xy_curve.py
+++ Mark_IV/Sintering/xy_curve.py
@@ -15,11 +15,12 @@ def xyCurve(x_dist=1, y_dist=1, x_circle=0, y_circle=1, rotation=False):
     radius = sqrt(x_circle * x_circle + y_circle * y_circle)
 
     # Get start and end angle on unit circle.
-    if x_circle == 0:
-        start_ang = math.pi / 2
-    else:
+    start_ang = math.pi / 2
+    end_ang = math.pi / 2
+    if x_circle != 0:
         start_ang = atan(y_circle / x_circle)
-    end_ang = atan((y_circle - y_dist) / (x_circle + float(x_dist)))
+    if x_circle + x_dist != 0:
+        end_ang = atan((y_circle - y_dist) / (x_circle + float(x_dist)))
 
     # Angles can be manually set for testing.
     # start_ang = math.pi / 3
@@ -45,8 +46,8 @@ def xyCurve(x_dist=1, y_dist=1, x_circle=0, y_circle=1, rotation=False):
         # Get current angle on unit circle.
         theta += angle_delta
 
-        x = x_prev - radius * abs(cos(theta))
-        y = y_prev - radius * abs(sin(theta))
+        x = (x_prev - radius * abs(cos(theta))) * (18 / num_segs)
+        y = (y_prev - radius * abs(sin(theta))) * (18 / num_segs)
 
         x_speed = speed_mod
         y_speed = speed_mod
