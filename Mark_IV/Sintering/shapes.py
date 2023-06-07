654ec0878a076bcc134a5f3c53a12643aedf56d6
JuddB <jbrockedgar@gmail.com>
2023-06-07 16:04:35 -0400

    added hexagon and hexagonal prism geometries.

--- Mark_IV/Sintering/shapes.py
+++ Mark_IV/Sintering/shapes.py
@@ -2,12 +2,13 @@ import RPi.GPIO as GPIO
 from xMove import xMove
 from yMove import yMove
 from zMove import zMove
+from xyMove import xyMove
 from xy_curve import xyCurve
+from math import sqrt
 from axisReset import axis_reset
 
 
 def box2d(x_dist=7, y_dist=5):
-    GPIO.cleanup()
     ar = axis_reset()
     ar.x_axis_reset()
     ar.y_axis_reset()
@@ -33,12 +34,10 @@ def box2d(x_dist=7, y_dist=5):
     
 
 def box3d(x_dist=14, y_dist=8, z_dist=8):
-    GPIO.cleanup()
     ar = axis_reset()
     ar.x_axis_reset()
     ar.y_axis_reset()
 
-    # Direction pin from controller
     focal_diameter = 1
     num_y_lines = int(round(y_dist / focal_diameter, 0))
     num_z_lines = int(round(z_dist / focal_diameter, 0))
@@ -64,8 +63,6 @@ def box3d(x_dist=14, y_dist=8, z_dist=8):
 
 
 def circle(radius=3, start_out=True):
-    GPIO.cleanup()
-
     # Diameter of focal point in cm.
     focal_diameter = 1
     num_layers = int(round(radius / focal_diameter, 0))
@@ -92,10 +89,43 @@ def circle(radius=3, start_out=True):
         GPIO.cleanup()
 
 
-def cylinder(radius=6, height=6):
-    GPIO.cleanup()
+def hexagon(width=5, start_out=True):
+    focal_diameter = 1
+    num_layers = int(round((width) / (2 * focal_diameter), 0))
+    if start_out:
+        width = focal_diameter * (num_layers - 0.5) * 2
+    else:
+        width = focal_diameter
+    side = width / 4
 
-    # Direction pin from controller
+    try:
+        for i in range(num_layers):
+            # Traces hexagon
+            xMove(side, True)
+            xyMove(side, side * sqrt(3))
+            xyMove(side * -1, side * sqrt(3))
+            xMove(side * 2, False)
+            xyMove(side * -1, side * sqrt(3) * -1)
+            xyMove(side, side * sqrt(3) * -1)
+            xMove(side, True)
+
+            if i == num_layers - 1:
+                break
+            
+            # Moves to start next hexagon
+            if start_out:
+                yMove(focal_diameter, True)
+                side -= focal_diameter / 2
+            else:
+                yMove(focal_diameter, False)
+                side += focal_diameter / 2
+        
+    except KeyboardInterrupt:
+        print("cleanup")
+        GPIO.cleanup()
+
+
+def cylinder(radius=6, height=6):
     focal_diameter = 1
     num_layers = int(round(height / focal_diameter, 0))
     start_out = True
@@ -117,10 +147,29 @@ def cylinder(radius=6, height=6):
         GPIO.cleanup()
 
 
-def semi_sphere(radius=6):
-    GPIO.cleanup()
+def hexagonal_prism(width=5, height=5):
+    focal_diameter = 1
+    num_layers = int(round(height / focal_diameter, 0))
+    start_out = True
 
-    # Direction pin from controller
+    # True moves linear actuator forward.
+    try:
+        for i in range(num_layers):
+            hexagon(width, start_out)
+
+            if i == num_layers - 1:
+                break
+            
+            zMove(focal_diameter, True)
+            start_out = not(start_out)
+            
+    # Once finished clean everything up
+    except KeyboardInterrupt:
+        print("cleanup")
+        GPIO.cleanup()
+
+
+def semi_sphere(radius=6):
     focal_diameter = 1
     num_layers = int(round(radius / focal_diameter, 0))
     start_out = True
@@ -144,12 +193,9 @@ def semi_sphere(radius=6):
 
 
 def bowl(radius=6):
-    GPIO.cleanup()
-
-    # Direction pin from controller
     focal_diameter = 1
 
-    # thickness defines how many layers thick the bowl will be.
+    # Thickness defines how many layers thick the bowl will be.
     thickness = 2
     num_layers = int(round(radius / focal_diameter, 0))
 
