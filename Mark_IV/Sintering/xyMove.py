235d7c01f05628e2e9c559a2aa1d7ca2c492dd9b
JuddB <jbrockedgar@gmail.com>
2023-06-28 12:54:59 -0400

    removes unnecessary code from xyMove.py

--- Mark_IV/Sintering/xyMove.py
+++ Mark_IV/Sintering/xyMove.py
@@ -1,6 +1,4 @@
-import time
 import multiprocessing as mp
-# from solarAlignment import solarTracking, solarElevationLogic, azimuthLogic
 from xMove import xMove
 from yMove import yMove
 # from zMove import zMove
@@ -44,24 +42,14 @@ def xyMove(x_dist=8, y_dist=8, z_dist=6):
     #     x_speed_mod = (x_dist / z_dist) * x_speed_mod
     #     y_speed_mod = (y_dist / z_dist) * y_speed_mod
 
-    # ar = axis_reset()
-    # ar.elevation_reset()
-    # if not(solarElevationLogic()):
-    #     exit()
-    # if not(azimuthLogic()):
-    #     exit()
-    # alignProc = mp.Process(target=solarTracking)
-
     # Starts moving x and y simultaneously in different processes.
     xProc = mp.Process(target=xMove, args=(x_dist, x_rotate, x_speed_mod))
     yProc = mp.Process(target=yMove, args=(y_dist, y_rotate, y_speed_mod))
-    # alignProc.start()
     xProc.start()
     yProc.start()
     # zMove(z_dist, z_dir, z_speed_mod)
     xProc.join()
     yProc.join()
-    # alignProc.terminate()
     print("X and Y finished!")
     
 
